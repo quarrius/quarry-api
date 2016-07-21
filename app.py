0acea39696a6a1b6ed8f02b0df848f05fc115a4e
aheadley <aheadley@waysaboutstuff.com>
2016-07-20 21:44:08 -0400

    update for external toybox

--- app.py
+++ app.py
@@ -8,15 +8,12 @@ import os.path
 import boto3
 import chalice
 
-from toybox import DB_INIT, World, User
+from toybox.models import World, User
 
 app = chalice.Chalice(app_name='quarry-api')
 app.debug = True
 
-ddb = boto3.resource('dynamodb')
 s3c = boto3.client('s3')
-
-DB_OBJ = DB_INIT('')
 
 KEY_EXPIRES_IN      = datetime.timedelta(hours=1)
 MIN_UPLOAD_SIZE     = 0
@@ -34,14 +31,7 @@ def map_get(map_id):
         raise chalice.NotFoundError('No map found for id: {}'.format(map_id))
     else:
         # return world data
-        return {}
-    tbl = ddb.Table('quarry-maps')
-    try:
-        map_data = tbl.get_item(Key={'id': map_id})['Item']
-    except KeyError as err:
-        raise chalice.NotFoundError('No map found for id: {}'.format(map_id))
-
-    return map_data
+        return {} #world.xattr['map_config']
 
 @app.route('/authorize-upload/world/{api_key}')
 def get_stream_upload_credentials(api_key):
@@ -58,15 +48,15 @@ def get_stream_upload_credentials(api_key):
                 'acl':                          'private',
                 'success_action_status':        200,
                 'x-amz-meta-quarry-api-key':    api_key,
-                'x-amz-meta-quarry-user-id':    user_id,
-                'x-amz-meta-quarry-world-id':   world_id,
+                'x-amz-meta-quarry-user-guid':    user.guid,
+                'x-amz-meta-quarry-world-guid':   world.guid,
             },
             Conditions=[
                 {'acl':                         'private'},
                 ['content-length-range',        MIN_UPLOAD_SIZE, MAX_UPLOAD_SIZE],
                 {'x-amz-meta-quarry-api-key':   api_key},
-                {'x-amz-meta-quarry-user-id':   user_id},
-                {'x-amz-meta-quarry-world-id':  world_id},
+                {'x-amz-meta-quarry-user-guid':   user.guid},
+                {'x-amz-meta-quarry-world-guid':  world.guid},
 
             ],
             ExpiresIn=KEY_EXPIRES_IN.seconds,
@@ -82,7 +72,7 @@ def get_archive_upload_credentials(api_key):
         user = world.user
         return s3c.generate_presigned_post(
             Bucket=UPLOAD_BUCKET,
-            Key='world-archives/{world_guid}.zip'.format(world_guid=world.guid),
+            Key='world-archives/{archive_name}.zip'.format(archive_name=world.api_key),
             Fields={
                 'acl':                          'private',
                 'success_action_status':        200,
