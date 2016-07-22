d42092d03b56f39b3bf84ca63bc0b6e0aa1f690c
Alex Headley <aheadley@maddogtechnology.com>
2016-07-22 16:22:39 -0400

    convert to zappa from chalice

--- app.py
+++ app.py
@@ -4,14 +4,18 @@
 import json
 import datetime
 import os.path
+import functools
 
+import flask
 import boto3
-import chalice
+from playhouse.flask_utils import FlaskDB, get_object_or_404
 
+from toybox.config import CFG
 from toybox.models import World, User
+from toybox.db import DATABASE
 
-app = chalice.Chalice(app_name='quarry-api')
-app.debug = True
+app = flask.Flask(__name__)
+flask_db = FlaskDB(app, DATABASE)
 
 s3c = boto3.client('s3')
 
@@ -20,73 +24,86 @@ MIN_UPLOAD_SIZE     = 0
 MAX_UPLOAD_SIZE     = 16 * 1024 * 1024
 UPLOAD_BUCKET       = 'quarrius-input'
 
+def json_response(orig_func):
+    @functools.wraps(orig_func)
+    def actual_func(*args, **kwargs):
+        return flask.jsonify(**orig_func(*args, **kwargs))
+    return actual_func
+
 @app.route('/')
+@json_response
 def index():
     return {'hello': 'world'}
 
-@app.route('/maps/{map_id}')
-def map_get(map_id):
-    world = World.select().where(World.map_token == map_id).first()
-    if world is None:
-        raise chalice.NotFoundError('No map found for id: {}'.format(map_id))
-    else:
-        # return world data
-        return {} #world.xattr['map_config']
+@app.route('/maps/<string:map_token>')
+@json_response
+def map_get(map_token):
+    world = get_object_or_404(World, World.map_token == map_token)
 
-@app.route('/authorize-upload/world/{api_key}')
+    return world.xattr['map_config']
+
+@app.route('/authorize-upload/world/<uuid:api_key>')
+@json_response
 def get_stream_upload_credentials(api_key):
-    world = World.select().where(World.api_key == api_key).first()
-    if world is None:
-        # TODO: change to 403 forbidden
-        raise chalice.NotFoundError('bad api_key')
-    else:
-        user = world.user
-        return s3c.generate_presigned_post(
-            Bucket=UPLOAD_BUCKET,
-            Key=os.path.join('worlds', user.guid, world.guid, '${filename}'),
-            Fields={
-                'acl':                          'private',
-                'success_action_status':        200,
-                'x-amz-meta-quarry-api-key':    api_key,
-                'x-amz-meta-quarry-user-guid':    user.guid,
-                'x-amz-meta-quarry-world-guid':   world.guid,
-            },
-            Conditions=[
-                {'acl':                         'private'},
-                ['content-length-range',        MIN_UPLOAD_SIZE, MAX_UPLOAD_SIZE],
-                {'x-amz-meta-quarry-api-key':   api_key},
-                {'x-amz-meta-quarry-user-guid':   user.guid},
-                {'x-amz-meta-quarry-world-guid':  world.guid},
+    world = get_object_or_404(World, World.api_key == api_key)
+    user = world.user
 
-            ],
-            ExpiresIn=KEY_EXPIRES_IN.seconds,
-        )
+    post_api_key = str(api_key)
+    post_user_guid = str(user.guid)
+    post_world_guid = str(world.guid)
+    return s3c.generate_presigned_post(
+        Bucket=UPLOAD_BUCKET,
+        Key=os.path.join('worlds', post_user_guid, post_world_guid, '${filename}'),
+        Fields={
+            'acl':                              'private',
+            'success_action_status':            200,
+            'x-amz-meta-quarry-api-key':    post_api_key,
+            'x-amz-meta-quarry-user-id':    post_user_guid,
+            'x-amz-meta-quarry-world-id':   post_world_guid,
+        },
+        Conditions=[
+            {'acl':                             'private'},
+            ['content-length-range',            MIN_UPLOAD_SIZE, MAX_UPLOAD_SIZE],
+            {'x-amz-meta-quarry-api-key':   post_api_key},
+            {'x-amz-meta-quarry-user-id':   post_user_guid},
+            {'x-amz-meta-quarry-world-id':  post_world_guid},
 
-@app.route('/authorize-upload/world-archive/{api_key}')
+        ],
+        ExpiresIn=KEY_EXPIRES_IN.seconds,
+    )
+
+@app.route('/authorize-upload/world-archive/<uuid:api_key>')
+@json_response
 def get_archive_upload_credentials(api_key):
+    # world = get_object_or_404(World, World.api_key == api_key)
     world = World.select().where(World.api_key == api_key).first()
-    if world is None:
-        # TODO: change to 403 forbidden
-        raise chalice.NotFoundError('bad api_key')
-    else:
-        user = world.user
-        return s3c.generate_presigned_post(
-            Bucket=UPLOAD_BUCKET,
-            Key='world-archives/{archive_name}.zip'.format(archive_name=world.api_key),
-            Fields={
-                'acl':                          'private',
-                'success_action_status':        200,
-                'x-amz-meta-quarry-api-key':    api_key,
-                'x-amz-meta-quarry-user-id':    user.guid,
-                'x-amz-meta-quarry-world-id':   world.guid,
-            },
-            Conditions=[
-                {'acl':                         'private'},
-                ['content-length-range',        MIN_UPLOAD_SIZE, MAX_UPLOAD_SIZE],
-                {'x-amz-meta-quarry-api-key':   api_key},
-                {'x-amz-meta-quarry-user-id':   user.guid},
-                {'x-amz-meta-quarry-world-id':  world.guid},
+    user = world.user
 
-            ],
-            ExpiresIn=KEY_EXPIRES_IN.seconds,
-        )
+    post_api_key = str(api_key)
+    post_user_guid = str(user.guid)
+    post_world_guid = str(world.guid)
+    return s3c.generate_presigned_post(
+        Bucket=UPLOAD_BUCKET,
+        Key='world-archives/{archive_name}.zip'.format(archive_name=post_api_key),
+        Fields={
+            'acl':                          'private',
+            'success_action_status':        200,
+            'x-amz-meta-quarry-api-key':    post_api_key,
+            'x-amz-meta-quarry-user-id':    post_user_guid,
+            'x-amz-meta-quarry-world-id':   post_world_guid,
+        },
+        Conditions=[
+            {'acl':                         'private'},
+            ['content-length-range',        MIN_UPLOAD_SIZE, MAX_UPLOAD_SIZE],
+            {'x-amz-meta-quarry-api-key':   post_api_key},
+            {'x-amz-meta-quarry-user-id':   post_user_guid},
+            {'x-amz-meta-quarry-world-id':  post_world_guid},
+
+        ],
+        ExpiresIn=KEY_EXPIRES_IN.seconds,
+    )
+
+if __name__ == '__main__':
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
+    app.run()
